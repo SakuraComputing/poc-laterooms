b699a07d1885f2430fc59dc2d6c6d04e13e98b9c
Ewan Bauld <ewan.bauld@gmail.com>
2018-10-23 12:22:24 +0100

    Add failing hotel reducer test

--- src/tests/components/LandingPage.spec.js
+++ src/tests/components/LandingPage.spec.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import LandingPage from '../components/LandingPage.js'
+import LandingPage from '../../components/LandingPage.js'
 
 describe('landing page component', () => {
 
---
+++ src/tests/reducers/hotels.spec.js
@@ -0,0 +1,23 @@
+import hotelReducer from '../../reducers/hotels';
+import hotels from '../../fixtures/hotels';
+
+describe('Hotel reducer', () => {
+    it('should set the default state', () => {
+        const state = hotelReducer(undefined, { type: '@@INIT'}); 
+        expect(state).toEqual([]);
+    });
+    it('should add a hotel', () => {
+        const hotel = {
+            name: 'New Hotel',
+            starRating: 4,
+            facilities: []
+        };
+        const action = {
+            type: 'ADD_HOTEL',
+            hotel
+        };
+        const state = hotelReducer(hotels, action);
+        expect(state).toEqual([...hotels, hotel]);
+    });
+})
+
