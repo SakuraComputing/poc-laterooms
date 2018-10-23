e048348675f0f28915f47cf5642872c4f79fef48
Ewan Bauld <ewan.bauld@gmail.com>
2018-10-23 13:44:21 +0100

    Add redux config

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import LandingPage from './components/LandingPage';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter';
+import getAppStore from './store/store';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = getAppStore();
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
